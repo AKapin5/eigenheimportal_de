3c1fc0d7db4593891c19fae17595e7ffaf0bb074
azhai <a.zhaivoron@seotm.com>
2022-07-29 14:08:57 +0300

    Fix

--- resources/views/apartment/list.blade.php
+++ resources/views/apartment/list.blade.php
@@ -3,8 +3,9 @@
         <section class="sales-offers">
             <h1 class="sales-offers__title">{{ $pageTitle }}</h1>
             <p class="sales-offers__subtitle">{{ $pageDescription }}</p>
-
-            <livewire:apartments :category="$category"/>
+            @isset($category)
+                <livewire:apartments :category="$category"/>
+            @endisset
         </section>
     </div>
 </x-layout>
