504f44c8d2a6f61379cc86add5d878232800cdf4
azhai <a.zhaivoron@seotm.com>
2022-08-09 14:48:19 +0300

    Admin panel optimizations

--- resources/views/apartment/list.blade.php
+++ resources/views/apartment/list.blade.php
@@ -2,7 +2,7 @@
     <div class="wrapper">
         <section class="sales-offers">
             <h1 class="sales-offers__title">{{ $pageTitle }}</h1>
-            <p class="sales-offers__subtitle">{!! nl2br($pageDescription) !!}</p>
+            <div class="sales-offers__subtitle">{!! $pageDescription !!}</div>
             @isset($category)
                 <livewire:apartments :category="$category"/>
             @endisset
